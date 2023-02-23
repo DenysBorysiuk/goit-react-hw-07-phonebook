f2e49457ba4987a5a538cb18040f1c7ca0ef065e
Denys Borysiuk <denisborisiuk@gmail.com>
2023-02-23 12:42:42 +0200

    add memo

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,26 +1,17 @@
 import { useSelector } from 'react-redux';
-import { selectContacts, selectFilter } from 'redux/selectors';
+import { selectVisibleContacts } from 'redux/selectors';
 import { ContactListItem } from 'components/ContactListItem/ContactListItem';
 import { List, Notice } from './ContactList.styled';
 
-const getVisibleContacts = (contacts, filter) => {
-  const normalizedFilter = filter.toLowerCase();
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter)
-  );
-};
-
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-  const visibleContacts = getVisibleContacts(contacts, filter);
+  const contacts = useSelector(selectVisibleContacts);
 
   return (
     <List>
       {contacts.length === 0 ? (
         <Notice>Contacts list is empty!</Notice>
       ) : (
-        visibleContacts.map(({ id, name, phone }) => (
+        contacts.map(({ id, name, phone }) => (
           <ContactListItem key={id} id={id} name={name} phone={phone} />
         ))
       )}
