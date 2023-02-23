40ae0e2840a103c816a856d9c403bfafcd9d10aa
Denys Borysiuk <denisborisiuk@gmail.com>
2023-02-23 12:43:36 +0200

    add memo

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -14,13 +14,3 @@ export const selectVisibleContacts = createSelector(
     );
   }
 );
-
-// export const selectVisibleContacts = state => {
-//   const contacts = selectContacts(state);
-//   const filter = selectFilter(state);
-//   const normalizedFilter = filter.toLowerCase();
-
-//   return contacts.filter(contact =>
-//     contact.name.toLowerCase().includes(normalizedFilter)
-//   );
-// };
